2b34a10c26eadfb63e0245c5bd367b1b2ad3d9a1
Bruno de Andrade <bruno.andrade23@gmail.com>
2018-11-02 17:14:33 -0300

    Initial commit

---
+++ Vega/Persistence/VegaDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vega.Models;
+
+namespace Vega.Persistence
+{
+    public class VegaDbContext : DbContext
+    {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Make> Makes { get; set; }
+    }
+}
--- Vega/Startup.cs
+++ Vega/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Vega.Persistence;
 
 namespace Vega
 {
@@ -24,13 +26,14 @@ namespace Vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
